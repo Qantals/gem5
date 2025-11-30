ea9383608c485a08aae6ced47eff5e74c5b43807
Qantals_ser <aheng_ah@126.com>
2025-11-30 15:58:57 +0800

    update

--- cmd.sh
+++ cmd.sh
@@ -4,22 +4,24 @@
 # ./build/VEGA_X86/gem5.opt configs/example/apu_se.py -n 3 --gpu --gfx-version=gfx900 -c gem5-resources/src/gpu/square/bin/square
 
 # my config
-OUTPUT_DIR=m5out_square_freqFolder/m5out_freq1.2.2
+OUTPUT_DIR=m5out_square_freqFolder/m5out_freq1.3.3
 mkdir -p "$OUTPUT_DIR"
 cp latency.txt "$OUTPUT_DIR"
 
-./build/VEGA_X86/gem5.opt \
+./build/VEGA_X86/gem5.fast \
 -d "$OUTPUT_DIR" \
 configs/example/apu_se.py \
 -n 4 \
 --latency-path latency.txt \
 --sys-clock 1GHz \
---CPUClock 2000MHz \
---gpu-clock 2000MHz \
+--CPUClock 3000MHz \
+--gpu-clock 3000MHz \
 --network garnet \
 --mem-channels 4 \
 --mem-size 512MB \
 --mem-type HBM_2000_4H_1x64 \
 --num-dirs 4 \
 -c gem5-resources/src/gpu/square/bin/square \
-2>&1 | tee "${OUTPUT_DIR}/print.log"
+> "${OUTPUT_DIR}/print.log" 2>&1 &
+
+# 2>&1 | tee "${OUTPUT_DIR}/print.log"
