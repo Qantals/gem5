7266c2ebf826210b44cbc9622c1158b16b65d9b7
Qantals_ser <aheng_ah@126.com>
2025-12-01 22:14:59 +0800

    update

--- cmd.sh
+++ cmd.sh
@@ -1,27 +1,49 @@
-# real run
-# ./build/VEGA_X86/gem5.opt configs/example/apu_se.py -n 3 -c gem5-resources/src/gpu/square/bin/square 2>&1 | tee print.log
-# gem5 2024 bootcamp
-# ./build/VEGA_X86/gem5.opt configs/example/apu_se.py -n 3 --gpu --gfx-version=gfx900 -c gem5-resources/src/gpu/square/bin/square
-
-# my config
-OUTPUT_DIR=m5out_square_freqFolder/m5out_freq1.3.3
+# test clock
+OUTPUT_DIR=m5out_freq2.2
 mkdir -p "$OUTPUT_DIR"
-cp latency.txt "$OUTPUT_DIR"
 
-./build/VEGA_X86/gem5.fast \
+./build/X86/gem5.fast \
 -d "$OUTPUT_DIR" \
-configs/example/apu_se.py \
--n 4 \
---latency-path latency.txt \
---sys-clock 1GHz \
---CPUClock 3000MHz \
---gpu-clock 3000MHz \
---network garnet \
---mem-channels 4 \
---mem-size 512MB \
---mem-type HBM_2000_4H_1x64 \
---num-dirs 4 \
--c gem5-resources/src/gpu/square/bin/square \
+configs/deprecated/example/se.py \
+--sys-clock 2GHz \
+--cpu-clock 2000MHz \
+-c gem5-resources/src/examples/matrix-multiply/matrix-multiply \
 > "${OUTPUT_DIR}/print.log" 2>&1 &
+
+# --cpu-type TimingSimpleCPU \
+# -n 4 \
+# --ruby \
+# --network garnet \
+# --mem-channels 4 \
+# --mem-size 512MB \
+# --mem-type HBM_2000_4H_1x64 \
+# --num-dirs 4 \
+
+
+
+
+
+
+
+# type 1
+# OUTPUT_DIR=m5out_square_freqFolder/m5out_freq1.3.3
+# mkdir -p "$OUTPUT_DIR"
+# cp latency.txt "$OUTPUT_DIR"
+
+# ./build/VEGA_X86/gem5.fast \
+# -d "$OUTPUT_DIR" \
+# configs/example/apu_se.py \
+# -n 4 \
+# --latency-path latency.txt \
+# --sys-clock 1GHz \
+# --CPUClock 3000MHz \
+# --gpu-clock 3000MHz \
+# --network garnet \
+# --mem-channels 4 \
+# --mem-size 512MB \
+# --mem-type HBM_2000_4H_1x64 \
+# --num-dirs 4 \
+# -c gem5-resources/src/gpu/square/bin/square \
+# > "${OUTPUT_DIR}/print.log" 2>&1 &
 
 # 2>&1 | tee "${OUTPUT_DIR}/print.log"
