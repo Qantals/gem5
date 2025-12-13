8a5ddbd295feff579593f1e6ebc996dcab8b2d20
Qantals_ser <aheng_ah@126.com>
2025-12-13 10:06:40 +0800

    update

--- cmd_DNNMark.sh
+++ cmd_DNNMark.sh
@@ -3,15 +3,15 @@
 
 # use docker with root user!
 # change --num-compute-units=4 require compile cachefiles again, check README.md
-OUTPUT_DIR=m5out_chiplet_DNNMark/
+OUTPUT_DIR=m5out_chiplet_benchmark/DNNMark
 trash "$OUTPUT_DIR"
 mkdir -p "$OUTPUT_DIR"
 cp latency.txt "$OUTPUT_DIR"
 
+# -r -e \
 docker run --rm -v ${PWD}:${PWD} -v ${PWD}/gem5-resources/src/gpu/DNNMark/cachefiles:/root/.cache/miopen/2.9.0 -w ${PWD} ghcr.io/gem5/gcn-gpu:v25-0-zyh \
 ./build/VEGA_X86/gem5.opt \
 -d "$OUTPUT_DIR" \
--r -e \
 configs/example/apu_se.py \
 --cpu-type X86O3CPU \
 -n 4 \
@@ -29,7 +29,8 @@ configs/example/apu_se.py \
 --num-dirs 4 \
 --benchmark-root=gem5-resources/src/gpu/DNNMark/build/benchmarks/test_fwd_softmax \
 -c dnnmark_test_fwd_softmax \
---options="-config gem5-resources/src/gpu/DNNMark/config_example/softmax_config.dnnmark -mmap gem5-resources/src/gpu/DNNMark/mmap.bin"
+--options="-config gem5-resources/src/gpu/DNNMark/config_example/softmax_config.dnnmark -mmap gem5-resources/src/gpu/DNNMark/mmap.bin" \
+2>&1 | tee "${OUTPUT_DIR}/print.log"
 
 docker run --rm -v ${PWD}:${PWD} -w ${PWD} ghcr.io/gem5/gcn-gpu:v25-0-zyh \
 chown -R $(id -u):$(id -g) "$OUTPUT_DIR"
