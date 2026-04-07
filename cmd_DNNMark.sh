205b374108e955e7cdd2e1e5e06c5c307f82da3b
Qantals_ser <aheng_ah@126.com>
2026-04-07 17:03:07 +0800

    Update DNNMark script: change output directory, modify benchmark options, and adjust latency parameters for improved testing setup

--- cmd_DNNMark.sh
+++ cmd_DNNMark.sh
@@ -3,14 +3,13 @@
 
 # use docker with root user!
 # change --num-compute-units=4 require compile cachefiles again, check README.md
-OUTPUT_DIR=m5out_chiplet_benchmark/DNNMark
-trash "$OUTPUT_DIR"
+OUTPUT_DIR=m5out_benchmarks/all/DNNMark/test_VGG
 mkdir -p "$OUTPUT_DIR"
-cp latency.txt "$OUTPUT_DIR"
+# cp latency.txt "$OUTPUT_DIR"
 
 # -r -e \
 docker run --rm -v ${PWD}:${PWD} -v ${PWD}/gem5-resources/src/gpu/DNNMark/cachefiles:/root/.cache/miopen/2.9.0 -w ${PWD} ghcr.io/gem5/gcn-gpu:v25-0-zyh \
-./build/VEGA_X86/gem5.opt \
+./build/VEGA_X86/gem5.fast \
 -d "$OUTPUT_DIR" \
 configs/example/apu_se.py \
 --cpu-type X86O3CPU \
@@ -21,15 +20,15 @@ configs/example/apu_se.py \
 --network garnet \
 --link-width-bits 64 \
 --chiplet-topo \
---latency-path latency.txt \
+--latency-val=4,5,6,6,5 \
 --chiplet-clock-domain \
 --chiplet-cdc \
 --mem-size 8GiB \
 --mem-type HBM_2000_4H_1x64 \
 --num-dirs 4 \
---benchmark-root=gem5-resources/src/gpu/DNNMark/build/benchmarks/test_fwd_softmax \
--c dnnmark_test_fwd_softmax \
---options="-config gem5-resources/src/gpu/DNNMark/config_example/softmax_config.dnnmark -mmap gem5-resources/src/gpu/DNNMark/mmap.bin" \
+--benchmark-root=gem5-resources/src/gpu/DNNMark/build/benchmarks/test_VGG \
+-c dnnmark_test_VGG \
+--options="-config gem5-resources/src/gpu/DNNMark/config_example/VGG.dnnmark -mmap gem5-resources/src/gpu/DNNMark/mmap.bin" \
 2>&1 | tee "${OUTPUT_DIR}/print.log"
 
 docker run --rm -v ${PWD}:${PWD} -w ${PWD} ghcr.io/gem5/gcn-gpu:v25-0-zyh \
